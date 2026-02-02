f056c0b8d7ddeca9a39b2210bcb0fda5c574baa1
numa0125t-ctrl <numa0125t@gmail.com>
2026-02-02 18:56:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // --- 0. Password Logic ---
 function checkPassword() {
-    const userPass = prompt("Enter the secret code to open your surprise(Hint 4digits pin):");
-    if (userPass === "0102") {
+    const userPass = prompt("Enter the secret code to open your surprise(sorry time out):");
+    if (userPass === "2255") {
         document.getElementById('main-content').classList.remove('hidden');
         startPlaylist();
     } else {
@@ -78,4 +78,5 @@ function createEmoji() {
     setTimeout(() => el.remove(), 6000);
 }
 setInterval(createEmoji, 750);
+
 
